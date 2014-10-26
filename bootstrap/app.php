1a3a2e1c134e9d98d7726ea77d80b62d3d3f207a
Benjamin Uzelac <ben@buzelac.com>
2014-10-26 15:12:54 -0500

    remove redundant comment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,17 +15,6 @@ $app = new Illuminate\Foundation\Application(
 	realpath(__DIR__.'/..')
 );
 
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| The first thing we will do is create a new Laravel application instance
-| which serves as the "glue" for all the components of Laravel, and is
-| the IoC container for the system binding all of the various parts.
-|
-*/
-
 $app->singleton(
 	'Illuminate\Contracts\Http\Kernel',
 	'App\Http\Kernel'
